010bc58823d0dfecf737d3fffe6d17ea263380cd
Eddie <eddiefor@kth.se>
2016-11-27 16:19:35 +0100

    maven och restful inlaggt

---
+++ src/main/java/Webservice/HelloWorld.java
@@ -0,0 +1,18 @@
+package Webservice;
+
+/**
+ * Created by Eddie on 2016-11-27.
+ */
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloWorld {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMessage() {
+        return "Hello world!";
+    }
+}
